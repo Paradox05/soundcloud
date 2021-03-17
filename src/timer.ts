41cef9bb3686ea06f720bc98c5a1f1886648d8b0
Roniel <ronielabreu22@gmail.com>
2021-03-17 19:22:53 -0400

    travis stuff

--- src/timer.ts
+++ src/timer.ts
@@ -1,5 +1,24 @@
-class Timer{
+export class Timer{
+    private timeRemaining: number;
     constructor(){
+        this.timeRemaining = 0;
         
     }
+
+    public startTimer() {
+        let timeRemaining = new Date().getTime();
+        return {
+            /** <integer>s e.g 2s etc. */
+            get seconds() {
+                const seconds = Math.ceil((new Date().getTime() - timeStart) / 1000) + 's';
+                return seconds;
+            },
+            /** Milliseconds e.g. 2000ms etc. */
+                get ms() {
+                    const ms = (new Date().getTime() - timeStart) + 'ms';
+                    return ms;
+                }
+        }
+    }
+    
 }
